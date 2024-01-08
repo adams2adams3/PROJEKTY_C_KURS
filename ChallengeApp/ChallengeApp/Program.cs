0ff5e7bf3befce7a7c586fe2257b2842266d5d97
adams2adams3 <adams2adams3@gmail.com>
2024-01-08 11:55:17 +0000

    Dzień 4 cchallenge app

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var plec = "kobieta";
+var wiek = 17;
+var imie = "Ewa";
+if (plec == "kobieta" && wiek < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30");
+}
+else if (imie == "Ewa" && wiek == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (plec == "mezczyzna" && wiek < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
