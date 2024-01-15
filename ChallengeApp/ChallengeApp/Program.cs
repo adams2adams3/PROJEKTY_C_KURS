f029378d921e6c80da7547f2c29637eaa51b5240
adams2adams3 <adams2adams3@gmail.com>
2024-01-15 13:07:29 +0000

    Zadanie domowe dzień 5 wersja poprawiona

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,69 +1,69 @@
 ﻿//Licznik cyfr w liczbie
 int number = 1111666000;
-string cyfryWLiczbie = number.ToString();
-char[] cyfry = cyfryWLiczbie.ToArray();
-Console.WriteLine(cyfry);
-int licznik0 = 0;
-int licznik1 = 0;
-int licznik2 = 0;
-int licznik3 = 0;
-int licznik4 = 0;
-int licznik5 = 0;
-int licznik6 = 0;
-int licznik7 = 0;
-int licznik8 = 0;
-int licznik9 = 0;
+string digitsInNumber = number.ToString();
+char[] digits = digitsInNumber.ToArray();
+Console.WriteLine(digits);
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-foreach (char cyfra in cyfry)
+foreach (char digit in digits)
 {
-    if (cyfra == '0')
+    if (digit == '0')
     {
-        licznik0++;
+        counter0++;
     }
-    else if (cyfra == '1')
+    else if (digit == '1')
     {
-        licznik1++;
+        counter1++;
     }
-    else if (cyfra == '2')
+    else if (digit == '2')
     {
-        licznik2++;
+        counter2++;
     }
-    else if (cyfra == '3')
+    else if (digit == '3')
     {
-        licznik3++;
+        counter3++;
     }
-    else if (cyfra == '4')
+    else if (digit == '4')
     {
-        licznik4++;
+        counter4++;
     }
-    if (cyfra == '5')
+    if (digit == '5')
     {
-        licznik5++;
+        counter5++;
     }
-    else if (cyfra == '6')
+    else if (digit == '6')
     {
-        licznik6++;
+        counter6++;
     }
-    else if (cyfra == '7')
+    else if (digit == '7')
     {
-        licznik7++;
+        counter7++;
     }
-    else if (cyfra == '8')
+    else if (digit == '8')
     {
-        licznik8++;
+        counter8++;
     }
-    else if (cyfra == '9')
+    else if (digit == '9')
     {
-        licznik9++;
+        counter9++;
     }
 }
-Console.WriteLine("LIczba zer = " + licznik0);
-Console.WriteLine("Liczba jedynek = " + licznik1);
-Console.WriteLine("Liczba dwojek = " + licznik2);
-Console.WriteLine("Liczba trojek = " + licznik3);
-Console.WriteLine("Liczba czworek = " + licznik4);
-Console.WriteLine("Liczba piatek = " + licznik5);
-Console.WriteLine("Liczba szostek = " + licznik6);
-Console.WriteLine("LIczba siodemek = " + licznik7);
-Console.WriteLine("Liczba osemek = " + licznik8);
-Console.WriteLine("Liczba dziewiatek = " + licznik9);
+Console.WriteLine("Liczba zer = " + counter0);
+Console.WriteLine("Liczba jedynek = " + counter1);
+Console.WriteLine("Liczba dwojek = " + counter2);
+Console.WriteLine("Liczba trojek = " + counter3);
+Console.WriteLine("Liczba czworek = " + counter4);
+Console.WriteLine("Liczba piatek = " + counter5);
+Console.WriteLine("Liczba szostek = " + counter6);
+Console.WriteLine("LIczba siodemek = " + counter7);
+Console.WriteLine("Liczba osemek = " + counter8);
+Console.WriteLine("Liczba dziewiatek = " + counter9);
